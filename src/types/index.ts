4fdc4296b7d4a725508152fd96ff428de09f217f
chenshone <1351766353@qq.com>
2020-05-28 19:12:43 +0800

    feat: 项目基础搭建，demo演示环境搭建

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'post'
+  | 'POST'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'put'
+  | 'PUT'
+  | 'delete'
+  | 'DELETE'
+  | 'patch'
+  | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
